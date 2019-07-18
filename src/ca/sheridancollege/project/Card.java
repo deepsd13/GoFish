7862c0606881398f38f3cbbe9a30ff6a215e3b5e
deepshah <shah98@sheridancollege.ca>
2019-07-18 11:18:35 -0400

    Updated class diagram

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -2,25 +2,82 @@
  * SYST 17796 Project Winter 2019 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
+ *
+ * @ Deep Shah June 26 2019
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+
 /**
  * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
+ * enough to be instantiated for any Card game. Students wishing to add to the
+ * code should remember to add themselves as a modifier.
+ *
  * @author dancye, 2018
  */
-public abstract class Card 
-{
+public class Card implements SuitValue {
     //default modifier for child classes
-    
+
+    private Value value;
+    private Suit suit;
+    private int deckSize = 52;
+
+    public Card(Suit s, Value v) {
+        suit = s;
+        value = v;
+    }
+
+    /*
+
+    public void generateDeck() {
+
+        for (Suit s : Suit.values()) {
+            for (Value v : Value.values()) {
+
+                Card c = new Card(s, v);
+                deck.add(c);
+            }
+        }
+    }
+     */ public boolean equals(Value value) {
+        if (this.getValue().equals(value)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public Value getValue() {
+        // TODO - implement Card.getValue
+        return value;
+    }
+
     /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     *
+     * @param value
      */
-    
-    @Override
-    public abstract String toString();
-    
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    /**
+     *
+     * @param suit
+     */
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public String toString() {
+
+        return getValue() + " of " + getSuit() + ", ";
+
+    }
+
 }
--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -7,36 +7,67 @@ package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 /**
- * A concrete class that represents any grouping of cards for a Game.
- * HINT, you might want to subclass this more than once.
- * The group of cards has a maximum size attribute which is flexible for reuse.
+ * A concrete class that represents any grouping of cards for a Game. HINT, you
+ * might want to subclass this more than once. The group of cards has a maximum
+ * size attribute which is flexible for reuse.
+ *
  * @author dancye
  */
-public class GroupOfCards 
-{
-   
+public class GroupOfCards extends Card {
+
     //The group of cards, stored in an ArrayList
-    private ArrayList <Card> cards;
+    private ArrayList<Card> cards = new ArrayList<>();
+    private static ArrayList<Card> deck = new ArrayList<Card>();
     private int size;//the size of the grouping
-    
-    public GroupOfCards(int givenSize)
-    {
+
+    public GroupOfCards(int givenSize, Suit suit, Value value) {
+        super(suit, value);
         size = givenSize;
     }
-    
-    /**
-     * A method that will get the group of cards as an ArrayList
-     * @return the group of cards.
-     */
-    public ArrayList<Card> showCards()
-    {
+
+  
+    public  ArrayList<Card> generateDeck() {
+
+        for (Suit s : Suit.values()) {
+            for (Value v : Value.values()) {
+
+                Card c = new Card(s, v);
+                deck.add(c);
+            }
+        }
+        return deck;
+    }
+
+    public ArrayList<Card> distributeCards() {
+
+        for (int i = 0; i < 5; i++) {
+
+            Random rn = new Random();
+            int rand = rn.nextInt(deck.size()-1) + 1;
+            cards.add((deck.get(rand)));
+            deck.remove(rand);
+
+        }
         return cards;
     }
-    
-    public void shuffle()
-    {
+
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    /**
+     * A method that will get the group of cards as an ArrayList
+     *
+     * @return the group of cards.
+     */
+    public ArrayList<Card> showCards() {
+        return cards;
+    }
+
+    public void shuffle() {
         Collections.shuffle(cards);
     }
 
@@ -53,5 +84,11 @@ public class GroupOfCards
     public void setSize(int givenSize) {
         size = givenSize;
     }
-    
+
+    @Override
+    public String toString() {
+
+        return "The remaining cards:" + deck;
+    }
+
 }//end class
